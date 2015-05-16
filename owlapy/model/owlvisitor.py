2db10999d67cc911f6f46db67c7758022838e8b4
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-05-16 16:46:20 +0200

    added visitor dummy classes

---
+++ owlapy/model/owlvisitor.py
@@ -0,0 +1,12 @@
+class OWLVisitor(object):
+    """TODO: implement and comment"""
+
+    def visit(self, object):
+        """
+        :param object: an owlapy.model.OWLObject object
+        """
+        raise NotImplementedError()
+
+class OWLVisitorEx(OWLVisitor):
+    """TODO: implement and comment"""
+
