e9625178290d9109f57f68305ea47ee30828b657
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-05-16 15:40:55 +0200

    updated imports in owlapy.model package __init__

--- owlapy/model/__init__.py
+++ owlapy/model/__init__.py
@@ -1,4 +1,7 @@
+from .iri import IRI
+from .exceptions import OWLRuntimeException
+from .nodeid import NodeID
 from .owlaxiom import OWLAxiom
 from .owlobject import OWLObject
+from .owlindividual import OWLIndividual
 
-from .iri import IRI
