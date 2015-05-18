0ea87881a429f17c4ecf4043c07eb6b85dda803f
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-05-18 23:48:49 +0200

    minor clean-ups

--- owlapy/collectioncontainer.py
+++ owlapy/collectioncontainer.py
@@ -1,7 +1,6 @@
 class CollectionContainer(object):
     def accept(self, coll_container_visitor):
         """
-
         :param coll_container_visitor: an owlapy.CollectionContainerVisitor
             object
         """
--- tests/collectioncontainer_tests.py
+++ tests/collectioncontainer_tests.py
@@ -3,6 +3,7 @@ import unittest
 from owlapy import CollectionContainer
 from owlapy import CollectionContainerVisitor
 
+
 class TestCollectionContainer(unittest.TestCase):
     def test_accept(self):
         coll_container = CollectionContainer()
