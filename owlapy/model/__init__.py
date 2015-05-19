53a6dbd5b4fb494abb440e14d75d7a38bef035aa
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-05-19 17:03:12 +0200

    added owldifferentindividualsaxiom dummy

--- owlapy/model/__init__.py
+++ owlapy/model/__init__.py
@@ -7,6 +7,7 @@ from .owlasymmetricobjectpropertyaxiom import OWLAsymmetricObjectPropertyAxiom
 from .owlaxiom import OWLAxiom
 from .owlobject import OWLObject
 from .owlclassaxiom import OWLClassAxiom
+from .owldifferentindividualsaxiom import OWLDifferentIndividualsAxiom
 from .owldisjointclassesaxiom import OWLDisjointClassesAxiom
 from .owldatapropertydomainaxiom import OWLDataPropertyDomainAxiom
 from .owlequivalentobjectpropertiesaxiom import \
