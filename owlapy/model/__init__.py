e350108974b6048eb07fd8d683c71f30a4e52fa9
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-05-19 22:06:43 +0200

    added owlquantifiedobjectrestriction dummy

--- owlapy/model/__init__.py
+++ owlapy/model/__init__.py
@@ -56,6 +56,7 @@ from .owlpropertyaxiom import OWLPropertyAxiom
 from .owlpropertydomainaxiom import OWLPropertyDomainAxiom
 from .owlpropertyrange import OWLPropertyRange
 from .owlpropertyrangeaxiom import OWLPropertyRangeAxiom
+from .owlquantifiedobjectrestriction import OWLQuantifiedObjectRestriction
 from .owlquantifiedrestriction import OWLQuantifiedRestriction
 from .owlreflexiveobjectpropertyaxiom import OWLReflexiveObjectPropertyAxiom
 from .owlrestriction import OWLRestriction
