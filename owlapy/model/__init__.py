fb061b4a0fad213707102fee8f141352488c34dc
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-05-19 16:44:14 +0200

    added owlequivalentobjectpropertiesaxiom dummy

--- owlapy/model/__init__.py
+++ owlapy/model/__init__.py
@@ -9,6 +9,8 @@ from .owlobject import OWLObject
 from .owlclassaxiom import OWLClassAxiom
 from .owldisjointclassesaxiom import OWLDisjointClassesAxiom
 from .owldatapropertydomainaxiom import OWLDataPropertyDomainAxiom
+from .owlequivalentobjectpropertiesaxiom import \
+    OWLEquivalentObjectPropertiesAxiom
 from .owlindividual import OWLIndividual
 from .owlindividualrelationshipaxiom import OWLIndividualRelationshipAxiom
 from .owllogicalaxiom import OWLLogicalAxiom
