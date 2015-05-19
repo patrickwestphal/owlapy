d9793eca8c1a0ae606eed1f63c0c446aa3a2043a
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-05-19 22:14:14 +0200

    added owlapy.model.owlobjectallvaluesfrom dummy

---
+++ owlapy/model/owlobjectallvaluesfrom.py
@@ -0,0 +1,12 @@
+from .owlquantifiedobjectrestriction import OWLQuantifiedObjectRestriction
+
+
+class OWLObjectAllValuesFrom(OWLQuantifiedObjectRestriction):
+    """TODO: implement"""
+
+    def __init__(self, property, filler):
+        """
+        :param property: an owlapy.model.OWLObjectPropertyExpression object
+        :param filler: an owlapy.model.OWLClassExpression object
+        """
+        super().__init__(property, filler)
