bf4f6359c459e0339f3b1e6f62aa855560c79445
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-05-19 14:09:00 +0200

    updated tests of owlapy.collectioncontainer module

--- tests/collectioncontainer_tests.py
+++ tests/collectioncontainer_tests.py
@@ -2,11 +2,19 @@ import unittest
 
 from owlapy import CollectionContainer
 from owlapy import CollectionContainerVisitor
+from owlapy import OWLEntityCollectionContainerCollector
+from owlapy.model import IRI
+from owlapy.model import NodeID
+from owlapy.model import OWLClass
+from owlapy.model import OWLAnonymousIndividual
 
 
 class TestCollectionContainer(unittest.TestCase):
     def test_accept(self):
+        entities = [OWLClass(IRI('http://ex.org/SomeCls'))]
+        anons = [OWLAnonymousIndividual(NodeID('_:23'))]
+        collector = OWLEntityCollectionContainerCollector(entities, anons)
         coll_container = CollectionContainer()
-        visitor = CollectionContainerVisitor()
+        visitor = CollectionContainerVisitor(collector)
         self.assertRaises(NotImplementedError, CollectionContainer.accept,
                           coll_container, visitor)
