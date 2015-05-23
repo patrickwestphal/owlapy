4173e7081fb9459f8e07d56d7853908e8abee372
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-05-23 15:13:03 +0200

    preliminary dummy fix in OWLDatafactory

--- owlapy/model/owldatafactory.py
+++ owlapy/model/owldatafactory.py
@@ -1,6 +1,14 @@
+from .iri import IRI
+from .owlobjectproperty import OWLObjectProperty
+
+
 class OWLDataFactory(object):
     """TODO: implement"""
 
     def __init__(self, cache=True, use_compression=False):
         # TODO: implement
         pass
+
+    def get_owl_object_property(self, foo):
+        """FIXME: this is just a dummy"""
+        return OWLObjectProperty(IRI('http://ex.org/justADummyProp'))
