f85346835e0fd45be017c9e73c826e437608dc64
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-05-24 18:53:22 +0200

    implemented ordering/hashing for OWLObjectProperty

--- owlapy/model/owlobjectproperty.py
+++ owlapy/model/owlobjectproperty.py
@@ -1,8 +1,10 @@
+from functools import total_ordering
 from .exceptions import OWLRuntimeException
 from .owlobjectpropertyexpression import OWLObjectPropertyExpression
 from .owlvisitor import OWLVisitorEx, OWLVisitor
 
 
+@total_ordering
 class OWLObjectProperty(OWLObjectPropertyExpression):
     """TODO: implement"""
 
@@ -12,6 +14,24 @@ class OWLObjectProperty(OWLObjectPropertyExpression):
         """
         super().__init__()
         self.iri = iri
+
+    def __eq__(self, other):
+        if super().__eq__(other):
+            if not isinstance(other, OWLObjectProperty):
+                return False
+
+            return other.iri == self.iri
+
+        return False
+
+    def __ge__(self, other):
+        return self.compare_to(other)
+
+    def __hash__(self):
+        return super().__hash__()
+
+    def compare_object_of_same_type(self, other):
+        return self.iri.compare_to(other.iri)
 
     def accept(self, visitor):
         if isinstance(visitor, OWLVisitorEx):
