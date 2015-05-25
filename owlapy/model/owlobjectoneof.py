8842d168dee429350e36d3e55dcfc374b35fc9ed
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-05-25 19:12:15 +0200

    added accept method in owlapy.model.OWLObjectOneOf

--- owlapy/model/owlobjectoneof.py
+++ owlapy/model/owlobjectoneof.py
@@ -1,4 +1,6 @@
+from .exceptions import OWLRuntimeException
 from .owlanonymousclassexpression import OWLAnonymousClassExpression
+from .owlvisitor import OWLVisitorEx, OWLVisitor
 
 
 class OWLObjectOneOf(OWLAnonymousClassExpression):
@@ -10,3 +12,21 @@ class OWLObjectOneOf(OWLAnonymousClassExpression):
         """
         super().__init__()
         self.individuals = values
+
+
+    def accept(self, visitor):
+        """
+        :param visitor: an object of one of the following classes:
+            - owlapy.model.OWLClassExpressionVisitor
+            - owlapy.model.OWLObjectVisitor
+            - owlapy.model.OWLClassExpressionVisitorEx
+            - owlapy.model.OWLObjectVisitorEx
+        """
+        if isinstance(visitor, OWLVisitorEx):
+            return visitor.visit(self)
+        elif isinstance(visitor, OWLVisitor):
+            visitor.visit(self)
+        else:
+            raise OWLRuntimeException('Can only accept instances of'
+                                      'owlapy.model.OWLVisitor or '
+                                      'owlapy.model.OWLVisitorEx')
