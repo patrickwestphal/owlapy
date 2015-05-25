eeb722216c15eabc06c25200c7d4e30847633bf8
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-05-25 17:25:51 +0200

    updated doc str in OWLDataAllValuesFrom

--- owlapy/model/owldataallvaluesfrom.py
+++ owlapy/model/owldataallvaluesfrom.py
@@ -9,7 +9,11 @@ class OWLDataAllValuesFrom(OWLQuantifiedDataRestriction):
     def __init__(self, property, filler):
         """
         :param property: an owlapy.model.OWLDataPropertyExpression object
-        :param filler: an owlapy.model.OWLDataRange object
+        :param filler: an owlapy.model.OWLDataRange object (i.e. an
+            owlapy.model.OWLDataComplementOf object, an
+            owlapy,model.OWLDataOneOf object, an owlapy.model.OWLDatatype
+            object, an owlapy.model.OWLDatatypeRestriction, or an
+            owlapy.model.OWLNaryDataRange object
         """
         super().__init__(property, filler)
 
