307fc43814edc9afa9ae9e7aa21091ce4883a457
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-05-25 17:00:29 +0200

    added accept method to OWLSameIndividualAxiom cls

--- owlapy/model/owlsameindividualaxiom.py
+++ owlapy/model/owlsameindividualaxiom.py
@@ -1,5 +1,7 @@
+from .exceptions import OWLRuntimeException
 from .axiomtype import AxiomType
 from .owlnaryindividualaxiom import OWLNaryIndividualAxiom
+from .owlvisitor import OWLVisitorEx, OWLVisitor
 
 
 class OWLSameIndividualAxiom(OWLNaryIndividualAxiom):
@@ -11,6 +13,24 @@ class OWLSameIndividualAxiom(OWLNaryIndividualAxiom):
         :param annotations: a set/list of owlapy.model.OWLAnnotation objects
         """
         super().__init__(individuals, annotations)
+
+    def accept(self, visitor):
+        """
+        :param visitor: an object of one of the following classes:
+            - owlapy.model.OWLAxiomVisitor
+            - owlapy.model.OWLObjectVisitor
+            - owlapy.model.OWLAxiomVisitorEx
+            - owlapy.model.OWLObjectVisitorEx
+        :return:
+        """
+        if isinstance(visitor, OWLVisitorEx):
+            return visitor.visit(self)
+        elif isinstance(visitor, OWLVisitor):
+            visitor.visit(self)
+        else:
+            raise OWLRuntimeException('Can only accept instances of'
+                                      'owlapy.model.OWLVisitor or '
+                                      'owlapy.model.OWLVisitorEx')
 
     @classmethod
     def get_axiom_type(cls):
