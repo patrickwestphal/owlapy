c0b7efdabc5ad88dfe440f74c894a347d0cc518f
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-05-25 23:06:34 +0200

    implemented ordering/hashing in OWLDatatype

--- owlapy/model/owldatatype.py
+++ owlapy/model/owldatatype.py
@@ -1,3 +1,5 @@
+from functools import total_ordering
+
 from .exceptions import OWLRuntimeException
 from .owldatarange import OWLDataRange
 from .owllogicalentity import OWLLogicalEntity
@@ -5,6 +7,7 @@ from .owlnamedobject import OWLNamedObject
 from .owlvisitor import OWLVisitorEx, OWLVisitor
 
 
+@total_ordering
 class OWLDatatype(OWLDataRange, OWLLogicalEntity, OWLNamedObject):
     """TODO: implement"""
 
@@ -14,6 +17,21 @@ class OWLDatatype(OWLDataRange, OWLLogicalEntity, OWLNamedObject):
         """
         super().__init__()
         self.iri = iri
+
+    def __eq__(self, other):
+        if super().__eq__(other) and isinstance(other, OWLDatatype):
+            return other.iri == self.iri
+
+        return False
+
+    def __ge__(self, other):
+        return self.compare_to(other) >= 0
+
+    def __hash__(self):
+        return super().__hash__()
+
+    def compare_object_of_same_type(self, other):
+        return self.iri.compare_to(other.iri)
 
     def accept(self, visitor):
         if isinstance(visitor, OWLVisitorEx):
