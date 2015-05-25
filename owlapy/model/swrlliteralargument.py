e8fb22a48fd5ebf4a65216ae8b26a6cc7b13da6f
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-05-25 22:16:14 +0200

    implemented ordering for SWRLLiteralArgument

--- owlapy/model/swrlliteralargument.py
+++ owlapy/model/swrlliteralargument.py
@@ -1,8 +1,11 @@
+from functools import total_ordering
+
 from .exceptions import OWLRuntimeException
 from .owlobject import OWLObject
 from .owlvisitor import OWLVisitorEx, OWLVisitor
 
 
+@total_ordering
 class SWRLLiteralArgument(OWLObject):
     """TODO: implement"""
 
@@ -12,6 +15,24 @@ class SWRLLiteralArgument(OWLObject):
         """
         super().__init__()
         self.literal = literal
+
+    def __eq__(self, other):
+        if id(self) == id(other):
+            return True
+
+        if not isinstance(other, SWRLLiteralArgument):
+            return False
+
+        return other.literal == self.literal
+
+    def __ge__(self, other):
+        return self.compare_to(other) >= 0
+
+    def __hash__(self):
+        return super().__hash__()
+
+    def compare_object_of_same_type(self, other):
+        return self.literal.compare_to(other.literal)
 
     def accept(self, visitor):
         if isinstance(visitor, OWLVisitorEx):
