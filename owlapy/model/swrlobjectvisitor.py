753623f82ee9f40620a9e29e91848821b0687950
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-05-27 23:28:37 +0200

    fixed SWRLObjectVisitor, addedSWRLObjectVisitorEX

--- owlapy/model/swrlobjectvisitor.py
+++ owlapy/model/swrlobjectvisitor.py
@@ -1,4 +1,40 @@
 class SWRLObjectVisitor(object):
+    """Marker class"""
 
-    def visit(self):
+    def visit(self, node):
+        """
+        :param node: an object of one of the following classes:
+            - owlapy.model.SWRLRule
+            - owlapy.model.SWRLClassAtom
+            - owlapy.model.SWRLDataRangeAtom
+            - owlapy.model.SWRLObjectPropertyAtom
+            - owlapy.model.SWRLDataPropertyAtom
+            - owlapy.model.SWRLBuiltInAtom
+            - owlapy.model.SWRLVariable
+            - owlapy.model.SWRLIndividualArgument
+            - owlapy.model.SWRLLiteralArgument
+            - owlapy.model.SWRLSameIndividualAtom
+            - owlapy.model.SWRLDifferentIndividualsAtom
+        """
+        raise NotImplementedError()
+
+
+class SWRLObjectVisitorEx(object):
+    """Marker class"""
+
+    def visit(self, node):
+        """
+        :param node: an object of one of the following classes:
+            - owlapy.model.SWRLRule
+            - owlapy.model.SWRLClassAtom
+            - owlapy.model.SWRLDataRangeAtom
+            - owlapy.model.SWRLObjectPropertyAtom
+            - owlapy.model.SWRLDataPropertyAtom
+            - owlapy.model.SWRLBuiltInAtom
+            - owlapy.model.SWRLVariable
+            - owlapy.model.SWRLIndividualArgument
+            - owlapy.model.SWRLLiteralArgument
+            - owlapy.model.SWRLSameIndividualAtom
+            - owlapy.model.SWRLDifferentIndividualsAtom
+        """
         raise NotImplementedError()
