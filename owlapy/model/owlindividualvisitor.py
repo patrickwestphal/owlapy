cf40f46a3f3cbf02f04585ef38bac65ec6ccb691
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-05-27 23:34:51 +0200

    added OWLIndividualVisitorEx

--- owlapy/model/owlindividualvisitor.py
+++ owlapy/model/owlindividualvisitor.py
@@ -8,3 +8,15 @@ class OWLIndividualVisitor(object):
             - owlapy.model.OWLAnonymousIndividual
         :return:
         """
+        raise NotImplementedError()
+
+
+class OWLIndividualVisitorEx(object):
+    """Marker class"""
+
+    def visit(self, individual):
+        """
+        :param individual: an object of one of the following classes:
+            - owlapy.model.OWLNamedIndividual
+        """
+        raise NotImplementedError()
