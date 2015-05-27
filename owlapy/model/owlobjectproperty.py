328dc995d060bf6437180b03aa456410ff25463e
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-05-28 01:46:24 +0200

    added accept methods for OWLObjectProperty

--- owlapy/model/owlobjectproperty.py
+++ owlapy/model/owlobjectproperty.py
@@ -1,7 +1,12 @@
 from functools import total_ordering
-from .exceptions import OWLRuntimeException
-from .owlobjectpropertyexpression import OWLObjectPropertyExpression
 
+from .owlobjectpropertyexpression import OWLObjectPropertyExpression
+from .owlentityvisitor import OWLEntityVisitor, OWLEntityVisitorEx
+from .owlnamedobjectvisitor import OWLNamedObjectVisitor
+from .owlobjectvisitor import OWLObjectVisitor, OWLObjectVisitorEx
+from .owlpropertyexpressionvisitor import OWLPropertyExpressionVisitor,\
+    OWLPropertyExpressionVisitorEx
+from owlapy.util import accept_default, accept_default_ex
 
 @total_ordering
 class OWLObjectProperty(OWLObjectPropertyExpression):
@@ -13,6 +18,16 @@ class OWLObjectProperty(OWLObjectPropertyExpression):
         """
         super().__init__()
         self.iri = iri
+
+        self._accept_fn_for_visitor_cls[OWLEntityVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLEntityVisitorEx] = accept_default_ex
+        self._accept_fn_for_visitor_cls[OWLNamedObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLObjectVisitorEx] = accept_default_ex
+        self._accept_fn_for_visitor_cls[OWLPropertyExpressionVisitor] = \
+            accept_default
+        self._accept_fn_for_visitor_cls[OWLPropertyExpressionVisitorEx] = \
+            accept_default_ex
 
     def __eq__(self, other):
         if super().__eq__(other):
