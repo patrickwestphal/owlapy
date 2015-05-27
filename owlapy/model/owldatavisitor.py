7b050172698f394e3a4cf8335fd3eef548f62fdc
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-05-27 23:06:22 +0200

    added OWLDataVisitorEx

--- owlapy/model/owldatavisitor.py
+++ owlapy/model/owldatavisitor.py
@@ -7,8 +7,25 @@ class OWLDataVisitor(OWLDataRangeVisitor):
     def visit(self, node):
         """
         :param node: an object of one of the following classes:
-            - OWLLiteral
-            - OWLFacetRestriction
+            - owlapy.model.OWLLiteral
+            - owlapy.model.OWLFacetRestriction
         :return: None
+        """
+        raise NotImplementedError()
+
+class OWLDataVisitorEx(object):
+    """Marker class"""
+
+    def visit(self, node):
+        """
+        :param node: an object of one of the following classes:
+            - owlapy.model.OWLDatatype
+            - owlapy.model.OWLDataComplementOf
+            - owlapy.model.OWLDataOneOf
+            - owlapy.model.OWLDataIntersectionOf
+            - owlapy.model.OWLDataUnionOf
+            - owlapy.model.OWLDatatypeRestriction
+            - owlapy.model.OWLLiteral
+            - owlapy.model.OWLFacetRestriction
         """
         raise NotImplementedError()
