89fb0941aa5696f9a17a4d73b0b88083c80de2dd
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-05-27 21:25:48 +0200

    updated some visitor classes

--- owlapy/model/owlannotationaxiomvisitor.py
+++ owlapy/model/owlannotationaxiomvisitor.py
@@ -1,13 +1,30 @@
-class OWLAnnotationAxiomVisitor(object):
+from .owlvisitor import OWLVisitor, OWLVisitorEx
+
+
+class OWLAnnotationAxiomVisitor(OWLVisitor):
     """Marker class"""
 
     def visit(self, axiom):
         """
-        :param axiom: objects of one of these classes:
+        :param axiom: an object of one of these classes:
             - owlapy.model.OWLAnnotationAssertionAxiom
             - owlapy.model.OWLSubAnnotationPropertyOfAxiom
             - owlapy.model.OWLAnnotationPropertyDomainAxiom
             - owlapy.model.OWLAnnotationPropertyRangeAxiom
         :return: None
+        """
+        raise NotImplementedError()
+
+
+class OWLAnnotationVisitorEx(OWLVisitorEx):
+    """Marker Class"""
+
+    def visit(self, axiom):
+        """
+        :param axiom: an object of one of these classes:
+            - owlapy.model.OWLAnnotationAssertionAxiom
+            - owlapy.model.OWLSubAnnotationPropertyOfAxiom
+            - owlapy.model.OWLAnnotationPropertyDomainAxiom
+            - owlapy.model.OWLAnnotationPropertyRangeAxiom
         """
         raise NotImplementedError()
--- owlapy/model/owlaxiomvisitor.py
+++ owlapy/model/owlaxiomvisitor.py
@@ -1,4 +1,5 @@
-from .owlannotationaxiomvisitor import OWLAnnotationAxiomVisitor
+from .owlannotationaxiomvisitor import OWLAnnotationAxiomVisitor, \
+    OWLAnnotationVisitorEx
 
 
 class OWLAxiomVisitor(OWLAnnotationAxiomVisitor):
@@ -43,5 +44,49 @@ class OWLAxiomVisitor(OWLAnnotationAxiomVisitor):
             - owlapy.model.OWLDatatypeDefinitionAxiom
             - owlapy.model.SWRLRule
         :return:
+        """
+        raise NotImplementedError()
+
+
+class OWLAxiomVisitorEx(OWLAnnotationVisitorEx):
+
+    def visit(self, axiom):
+        """
+        :param axiom: an object of one of the following classes:
+            - owlapy.model.OWLSubClassOfAxiom
+            - owlapy.model.OWLNegativeObjectPropertyAssertionAxiom
+            - owlapy.model.OWLAsymmetricObjectPropertyAxiom
+            - owlapy.model.OWLReflexiveObjectPropertyAxiom
+            - owlapy.model.OWLDisjointClassesAxiom
+            - owlapy.model.OWLDataPropertyDomainAxiom
+            - owlapy.model.OWLObjectPropertyDomainAxiom
+            - owlapy.model.OWLEquivalentObjectPropertiesAxiom
+            - owlapy.model.OWLNegativeDataPropertyAssertionAxiom
+            - owlapy.model.OWLDifferentIndividualsAxiom
+            - owlapy.model.OWLDisjointDataPropertiesAxiom
+            - owlapy.model.OWLDisjointObjectPropertiesAxiom
+            - owlapy.model.OWLObjectPropertyRangeAxiom
+            - owlapy.model.OWLObjectPropertyAssertionAxiom
+            - owlapy.model.OWLFunctionalObjectPropertyAxiom
+            - owlapy.model.OWLSubObjectPropertyOfAxiom
+            - owlapy.model.OWLDisjointUnionAxiom
+            - owlapy.model.OWLDeclarationAxiom
+            - owlapy.model.OWLAnnotationAssertionAxiom
+            - owlapy.model.OWLSymmetricObjectPropertyAxiom
+            - owlapy.model.OWLDataPropertyRangeAxiom
+            - owlapy.model.OWLFunctionalDataPropertyAxiom
+            - owlapy.model.OWLEquivalentDataPropertiesAxiom
+            - owlapy.model.OWLClassAssertionAxiom
+            - owlapy.model.OWLEquivalentClassesAxiom
+            - owlapy.model.OWLDataPropertyAssertionAxiom
+            - owlapy.model.OWLTransitiveObjectPropertyAxiom
+            - owlapy.model.OWLIrreflexiveObjectPropertyAxiom
+            - owlapy.model.OWLSubDataPropertyOfAxiom
+            - owlapy.model.OWLInverseFunctionalObjectPropertyAxiom
+            - owlapy.model.OWLSubPropertyChainOfAxiom
+            - owlapy.model.OWLInverseObjectPropertiesAxiom
+            - owlapy.model.OWLHasKeyAxiom
+            - owlapy.model.OWLDatatypeDefinitionAxiom
+            - owlapy.model.SWRLRule
         """
         raise NotImplementedError()
