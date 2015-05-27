44f1165ece21595e4824ba2eff89b24c82ed8944
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-05-27 12:22:56 +0200

    added accept and super cls to OWLAxiom

--- owlapy/model/owlaxiom.py
+++ owlapy/model/owlaxiom.py
@@ -1,7 +1,10 @@
+from .exceptions import OWLRuntimeException
 from .owlobject import OWLObject
+from owlapy.collectioncontainer import CollectionContainer
+from .owlvisitor import OWLVisitorEx, OWLVisitor
 
 
-class OWLAxiom(OWLObject):
+class OWLAxiom(OWLObject, CollectionContainer):
     """Represents an Axiom in the OWL 2 Specification. An OWL ontology
     contains a set of axioms. These axioms can be annotation axioms,
     declaration axioms, imports axioms or logical axioms
@@ -17,3 +20,17 @@ class OWLAxiom(OWLObject):
             self.annotations.sort()
         else:
             self.annotations = []
+
+    def accept(self, visitor):
+        """
+        :param visitor: an owlapy.CollectionContainerVisitor object
+        """
+        if isinstance(visitor, OWLVisitorEx):
+            return visitor.visit(self)
+        elif isinstance(visitor, OWLVisitor):
+            visitor.visit(self)
+        else:
+            raise OWLRuntimeException('Can only accept instances of'
+                                      'owlapy.model.OWLVisitor or '
+                                      'owlapy.model.OWLVisitorEx')
+
