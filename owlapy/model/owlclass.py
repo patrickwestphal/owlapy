3a88aec4438d8d909865cd8a6745d0e68e2fb7a7
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-05-28 01:22:50 +0200

    added accept methods for OWLClass

--- owlapy/model/owlclass.py
+++ owlapy/model/owlclass.py
@@ -1,6 +1,13 @@
 from .owlclassexpression import OWLClassExpression
+from .owlclassexpressionvisitor import OWLClassExpressionVisitor, \
+    OWLClassExpressionVisitorEx
+from .owlentityvisitor import OWLEntityVisitor, OWLEntityVisitorEx
 from .owllogicalentity import OWLLogicalEntity
 from .owlnamedobject import OWLNamedObject
+from .owlnamedobjectvisitor import OWLNamedObjectVisitor
+from .owlobjectvisitor import OWLObjectVisitor, OWLObjectVisitorEx
+
+from owlapy.util import accept_default, accept_default_ex
 
 
 class OWLClass(OWLClassExpression, OWLLogicalEntity, OWLNamedObject):
@@ -12,6 +19,15 @@ class OWLClass(OWLClassExpression, OWLLogicalEntity, OWLNamedObject):
         """
         super().__init__()
         self.iri = iri
+
+        self._accept_fn_for_visitor_cls[OWLClassExpressionVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLClassExpressionVisitorEx] = \
+            accept_default_ex
+        self._accept_fn_for_visitor_cls[OWLEntityVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLEntityVisitorEx] = accept_default_ex
+        self._accept_fn_for_visitor_cls[OWLNamedObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLObjectVisitorEx] = accept_default_ex
 
     def compare_object_of_same_type(self, other):
         """
