24b1f785b40eac14ee0a5bae68961518dd563772
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-05-28 01:20:53 +0200

    added accept methods for OWLAnnotationProperty

--- owlapy/model/owlannotationproperty.py
+++ owlapy/model/owlannotationproperty.py
@@ -1,4 +1,8 @@
 from .owlentity import OWLEntity
+from .owlentityvisitor import OWLEntityVisitor, OWLEntityVisitorEx
+from .owlnamedobjectvisitor import OWLNamedObjectVisitor
+from .owlobjectvisitor import OWLObjectVisitor, OWLObjectVisitorEx
+from owlapy.util import accept_default, accept_default_ex
 
 
 class OWLAnnotationProperty(OWLEntity):
@@ -10,6 +14,12 @@ class OWLAnnotationProperty(OWLEntity):
         """
         super().__init__()
         self.iri = iri
+
+        self._accept_fn_for_visitor_cls[OWLEntityVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLEntityVisitorEx] = accept_default_ex
+        self._accept_fn_for_visitor_cls[OWLNamedObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLObjectVisitorEx] = accept_default_ex
 
     def compare_object_of_same_type(self, other):
         """
