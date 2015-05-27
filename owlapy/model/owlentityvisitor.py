79d79ef9e1d9f1a167c7a4c071106d006b5c7383
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-05-27 23:11:41 +0200

    added OWLEntityVisitorEx

--- owlapy/model/owlentityvisitor.py
+++ owlapy/model/owlentityvisitor.py
@@ -13,3 +13,19 @@ class OWLEntityVisitor(object):
         :return: None
         """
         raise NotImplementedError()
+
+
+class OWLEntityVisitorEx(object):
+    """Marker class"""
+
+    def visit(self, visitee):
+        """
+        :param visitee: an object of one of the following classes:
+            - owlapy.model.OWLClass
+            - owlapy.model.OWLObjectProperty
+            - owlapy.model.OWLDataProperty
+            - owlapy.model.OWLNamedIndividual
+            - owlapy.model.OWLDatatype
+            - owlapy.model.OWLAnnotationProperty
+        """
+        raise NotImplementedError()
