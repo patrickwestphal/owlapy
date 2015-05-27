f79053ebfb4bdd038533a3283cdb634868f165cb
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-05-27 23:45:29 +0200

    added OWLPropertyExpressionVisitorEx

--- owlapy/model/owlpropertyexpressionvisitor.py
+++ owlapy/model/owlpropertyexpressionvisitor.py
@@ -10,3 +10,15 @@ class OWLPropertyExpressionVisitor(object):
         :return: None
         """
         raise NotImplementedError()
+
+class OWLPropertyExpressionVisitorEx(object):
+    """Marker class"""
+
+    def visit(self, propery):
+        """
+        :param propery: an object of one of the following classes:
+            - owlapy.model.OWLObjectProperty
+            - owlapy.model.OWLObjectInverseOf
+            - owlapy.model.OWLDataProperty
+        """
+        raise NotImplementedError()
