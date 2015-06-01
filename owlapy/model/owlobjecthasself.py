3af1ea9a0bde974aed87a534eb04e2dcf6c033db
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-06-01 16:35:57 +0200

    added accept methods to OWLObjectHasSelf

--- owlapy/model/owlobjecthasself.py
+++ owlapy/model/owlobjecthasself.py
@@ -1,4 +1,8 @@
+from .owlclassexpressionvisitor import OWLClassExpressionVisitor, \
+    OWLClassExpressionVisitorEx
+from .owlobjectvisitor import OWLObjectVisitor, OWLObjectVisitorEx
 from .owlrestriction import OWLRestriction
+from owlapy.util import accept_default, accept_default_ex
 
 
 class OWLObjectHasSelf(OWLRestriction):
@@ -9,3 +13,10 @@ class OWLObjectHasSelf(OWLRestriction):
         :param property: an owlapy.model.OWLObjectPropertyExpression object
         """
         super().__init__(property)
+
+        self._accept_fn_for_visitor_cls[OWLClassExpressionVisitor] = \
+            accept_default
+        self._accept_fn_for_visitor_cls[OWLClassExpressionVisitorEx] = \
+            accept_default_ex
+        self._accept_fn_for_visitor_cls[OWLObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLObjectVisitorEx] = accept_default_ex
