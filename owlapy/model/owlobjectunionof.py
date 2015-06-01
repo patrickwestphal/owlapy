82e1640c81b4de385456f2fca2054e1a9d49fe3d
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-06-01 17:05:52 +0200

    added accept methods to OWLObjectUnionOf

--- owlapy/model/owlobjectunionof.py
+++ owlapy/model/owlobjectunionof.py
@@ -1,6 +1,10 @@
 from functools import total_ordering
-from .exceptions import OWLRuntimeException
+
+from .owlclassexpressionvisitor import OWLClassExpressionVisitor, \
+    OWLClassExpressionVisitorEx
 from .owlnarybooleanclassexpression import OWLNaryBooleanClassExpression
+from .owlobjectvisitor import OWLObjectVisitor, OWLObjectVisitorEx
+from owlapy.util import accept_default, accept_default_ex
 
 
 @total_ordering
@@ -12,6 +16,13 @@ class OWLObjectUnionOf(OWLNaryBooleanClassExpression):
         :param operands: a set of owlapy.model.OWLClassExpression objects
         """
         super().__init__(operands)
+
+        self._accept_fn_for_visitor_cls[OWLClassExpressionVisitor] = \
+            accept_default
+        self._accept_fn_for_visitor_cls[OWLClassExpressionVisitorEx] = \
+            accept_default_ex
+        self._accept_fn_for_visitor_cls[OWLObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLObjectVisitorEx] = accept_default_ex
 
     def __eq__(self, other):
         if super().__eq__(other):
