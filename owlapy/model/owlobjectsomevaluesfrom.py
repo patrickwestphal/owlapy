9030a5cfd1a638a2f072bf214cd9b2468456f05d
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-06-01 16:59:09 +0200

    added accept methods to OWLObjectSomeValuesFrom

--- owlapy/model/owlobjectsomevaluesfrom.py
+++ owlapy/model/owlobjectsomevaluesfrom.py
@@ -1,4 +1,8 @@
+from .owlclassexpressionvisitor import OWLClassExpressionVisitor, \
+    OWLClassExpressionVisitorEx
+from .owlobjectvisitor import OWLObjectVisitor, OWLObjectVisitorEx
 from .owlquantifiedobjectrestriction import OWLQuantifiedObjectRestriction
+from owlapy.util import accept_default, accept_default_ex
 
 
 class OWLObjectSomeValuesFrom(OWLQuantifiedObjectRestriction):
@@ -10,3 +14,10 @@ class OWLObjectSomeValuesFrom(OWLQuantifiedObjectRestriction):
         :param filler: an owlapy.model.OWLClassExpression object
         """
         super().__init__(property, filler)
+
+        self._accept_fn_for_visitor_cls[OWLClassExpressionVisitor] = \
+            accept_default
+        self._accept_fn_for_visitor_cls[OWLClassExpressionVisitorEx] = \
+            accept_default_ex
+        self._accept_fn_for_visitor_cls[OWLObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLObjectVisitorEx] = accept_default_ex
