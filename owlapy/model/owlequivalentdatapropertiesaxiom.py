9f36d0957c61d5d173c7da0d69b1fa68c89fcb9c
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-06-01 17:53:20 +0200

    added accept methods to OWLEquiv.DataProp.Axiom

--- owlapy/model/owlequivalentdatapropertiesaxiom.py
+++ owlapy/model/owlequivalentdatapropertiesaxiom.py
@@ -1,5 +1,9 @@
 from .axiomtype import AxiomType
+from .owlclassexpressionvisitor import OWLClassExpressionVisitor, \
+    OWLClassExpressionVisitorEx
 from .owlnarypropertyaxiom import OWLNaryPropertyAxiom
+from .owlobjectvisitor import OWLObjectVisitor, OWLObjectVisitorEx
+from owlapy.util import accept_default, accept_default_ex
 
 
 class OWLEquivalentDataPropertiesAxiom(OWLNaryPropertyAxiom):
@@ -13,6 +17,13 @@ class OWLEquivalentDataPropertiesAxiom(OWLNaryPropertyAxiom):
         :return:
         """
         super().__init__(properties, annotations)
+
+        self._accept_fn_for_visitor_cls[OWLClassExpressionVisitor] = \
+            accept_default
+        self._accept_fn_for_visitor_cls[OWLClassExpressionVisitorEx] = \
+            accept_default_ex
+        self._accept_fn_for_visitor_cls[OWLObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLObjectVisitorEx] = accept_default_ex
 
     @classmethod
     def get_axiom_type(cls):
