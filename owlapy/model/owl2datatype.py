98cd85495aa4520ee2b61606ec22579d308b9567
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-06-01 13:55:59 +0200

    added owlapy.model.owl2datatype + tests

---
+++ owlapy/model/owl2datatype.py
@@ -0,0 +1,218 @@
+from .datarangetype import DataRangeType
+from .entitytype import EntityType
+from .exceptions import NoneValueException, OWLRuntimeException
+from .owldatarangevisitor import OWLDataRangeVisitor, OWLDataRangeVisitorEx
+from .owldatatype import OWLDatatype
+from .owldatavisitor import OWLDataVisitor, OWLDataVisitorEx
+from .owlentityvisitor import OWLEntityVisitor, OWLEntityVisitorEx
+from .owlnamedobjectvisitor import OWLNamedObjectVisitor
+from .owlobjectvisitor import OWLObjectVisitor, OWLObjectVisitorEx
+from owlapy.util import accept_default, accept_default_ex
+from owlapy.util.decorators import ClassProperty
+import owlapy.vocab.owl2datatype as vocab_owl2datatype
+import owlapy.util
+
+
+class OWL2Datatype(OWLDatatype):
+
+    def __init__(self, owl2_datatype):
+        if owl2_datatype is None:
+            raise NoneValueException('owl2_datatype must not be null')
+
+        self.owl2_datatype = owl2_datatype
+        super().__init__(owl2_datatype.iri)
+
+        self._accept_fn_for_visitor_cls[OWLDataRangeVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLDataRangeVisitorEx] = \
+            accept_default_ex
+        self._accept_fn_for_visitor_cls[OWLDataVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLDataVisitorEx] = accept_default_ex
+        self._accept_fn_for_visitor_cls[OWLEntityVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLEntityVisitorEx] = accept_default_ex
+        self._accept_fn_for_visitor_cls[OWLNamedObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLObjectVisitorEx] = accept_default_ex
+
+    def __str__(self):
+        return self.to_string_id()
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, other):
+        if id(self) == id(other):
+            return True
+
+        if not isinstance(other, OWLDatatype):
+            return False
+
+        return self.owl2_datatype.iri == other.iri
+
+    def __hash__(self):
+        return super().__hash__()
+
+    @ClassProperty
+    @classmethod
+    def instance_map(cls):
+        if not hasattr(cls, '_instance_map'):
+            cls._instance_map = {}
+            for dtype in vocab_owl2datatype.OWL2Datatype:
+                cls._instance_map[dtype] = OWL2Datatype(dtype)
+
+        return cls._instance_map
+
+    @classmethod
+    def get_datatype(cls, datatype):
+        """
+        :param datatype: an owlapy.vocab.OWL2Datatype object
+        :return: an owlapy.model.OWLDatatype object
+        """
+        return cls.instance_map[datatype]
+
+    def get_built_in_type(self):
+        return self.owl2_datatype
+
+    def is_string(self):
+        return self.owl2_datatype == vocab_owl2datatype.OWL2Datatype.XSD_STRING
+
+    def is_integer(self):
+        return self.owl2_datatype == vocab_owl2datatype.OWL2Datatype.XSD_INTEGER
+
+    def is_float(self):
+        return self.owl2_datatype == vocab_owl2datatype.OWL2Datatype.XSD_FLOAT
+
+    def is_double(self):
+        return self.owl2_datatype == vocab_owl2datatype.OWL2Datatype.XSD_DOUBLE
+
+    def is_boolean(self):
+        return self.owl2_datatype == vocab_owl2datatype.OWL2Datatype.XSD_BOOLEAN
+
+    def is_rdf_plain_literal(self):
+        return self.owl2_datatype == \
+            vocab_owl2datatype.OWL2Datatype.RDF_PLAIN_LITERAL
+
+    def is_datatype(self):
+        return True
+
+    def is_top_datatype(self):
+        return self.owl2_datatype == \
+            vocab_owl2datatype.OWL2Datatype.RDFS_LITERAL
+
+    def as_owl_datatype(self):
+        return self
+
+    def get_data_range_type(self):
+        return DataRangeType.DATATYPE
+
+    def get_entity_type(self):
+        return EntityType.DATATYPE
+
+    def get_owl_entity(self, entity_type):
+        from owlapy.model import OWLObject
+        return OWLObject.get_owl_entity(
+            entity_type, vocab_owl2datatype.OWL2Datatype.RDF_PLAIN_LITERAL.iri)
+
+    def is_type(self, other_entity_type):
+        return other_entity_type == EntityType.DATATYPE
+
+    def get_annotations(self, ontology, annotation_property=None):
+        """
+        :param ontology: an owlapy.model.OWLOntology object
+        :param annotation_property: an owlapy.model.OWLAnnotationProperty object
+        :return: a set of owlapy.model.OWLAnnotation objects
+        """
+        return owlapy.util.get_annotations(
+            self, {ontology}, annotation_property)
+
+    def get_annotation_assertion_axioms(self, ontology):
+        """
+        :param ontology: an owlapy.model.OWLOntology object
+        :return: a set of owlapy.model.OWLAnnotationAssertionAxiom objects
+        """
+        return owlapy.util.get_annotation_axioms(self, {ontology})
+
+    def is_built_in(self):
+        return True
+
+    def is_owl_class(self):
+        return False
+
+    def as_owl_class(self):
+        raise OWLRuntimeException('Not an OWLClass')
+
+    def is_owl_object_property(self):
+        return False
+
+    def as_owl_object_property(self):
+        raise OWLRuntimeException('Not an OWLObjectProperty')
+
+    def is_owl_data_property(self):
+        return False
+
+    def as_owl_data_property(self):
+        raise OWLRuntimeException('Not an OWLDataProperty')
+
+    def is_owl_named_individual(self):
+        return False
+
+    def as_owl_named_individual(self):
+        raise OWLRuntimeException('Not an OWLNamedIndividual')
+
+    def is_owl_datatype(self):
+        return True
+
+    def is_owl_annotation_property(self):
+        return False
+
+    def as_owl_annotation_property(self):
+        raise OWLRuntimeException('Not an OWLAnnotationProperty')
+
+    def to_string_id(self):
+        return str(self.owl2_datatype.iri)
+
+    def get_referencing_axioms(self, ontology, include_imports=False):
+        """
+        :param ontology: an owlapy.model.OWLOntology object
+        :return: a set of owlapy.model.OWLAxiom objects
+        """
+        return ontology.get_referencing_axioms(self, include_imports)
+
+    def get_signature(self):
+        return {self}
+
+    def contains_entity_in_signature(self, entity):
+        """
+        :param entity: an owlapy.model.OWLEntity object
+        """
+        return self == entity
+
+    def get_anonymous_individuals(self):
+        return set()
+
+    def get_classes_in_signature(self):
+        return set()
+
+    def get_data_properties_in_signature(self):
+        return set()
+
+    def get_object_properties_in_signature(self):
+        return set()
+
+    def get_individuals_in_signature(self):
+        return set()
+
+    def get_datatypes_in_signature(self):
+        # note: in the original OWLAPI code, self is casted to model.OWLDatatype
+        # hence, the type will be different here (i.e. OWL2Datatype instead of
+        # OWLDatatype)
+        return {self}
+
+    def get_nested_class_expressions(self):
+        return set()
+
+    def is_top_entity(self):
+        return self.owl2_datatype == \
+            vocab_owl2datatype.OWL2Datatype.RDF_PLAIN_LITERAL
+
+    def is_bottom_entity(self):
+        return False
---
+++ tests/model/owl2datatype_tests.py
@@ -0,0 +1,337 @@
+import unittest
+
+from owlapy.model import DataRangeType
+from owlapy.model import EntityType
+from owlapy.model import IRI
+from owlapy.model import OWL2Datatype
+from owlapy.model import OWLDatatype
+from owlapy.model import OWLObjectVisitor
+from owlapy.model import OWLObjectVisitorEx
+from owlapy.model import OWLRuntimeException
+from owlapy.vocab.owl2datatype import OWL2Datatype as OWL2DtypeVoc
+
+
+class OWL2DatatypeTest(unittest.TestCase):
+
+    def test___init__(self):
+        dtype = OWL2DtypeVoc.XSD_BASE_64_BINARY
+        owl2datatype = OWL2Datatype(dtype)
+
+        self.assertEqual(dtype, owl2datatype.owl2_datatype)
+
+    def test___str__(self):
+        iri_str = str(OWL2DtypeVoc.OWL_RATIONAL.iri)
+        self.assertEqual(iri_str, str(OWL2Datatype(OWL2DtypeVoc.OWL_RATIONAL)))
+
+    def test___eq__01(self):
+        """same object"""
+        self.assertEqual(
+            OWL2Datatype(OWL2DtypeVoc.XSD_INTEGER),
+            OWL2Datatype(OWL2DtypeVoc.XSD_INTEGER))
+
+    def test___eq__02(self):
+        """same IRI string, model.OWLDatatype class"""
+        dtype = OWLDatatype(IRI('http://www.w3.org/2001/XMLSchema#integer'))
+        self.assertEqual(dtype, OWL2Datatype(OWL2DtypeVoc.XSD_INTEGER))
+
+    def test___eq__03(self):
+        """same class, differing IRI"""
+        self.assertNotEqual(
+            OWL2Datatype(OWL2DtypeVoc.XSD_INT),
+            OWL2Datatype(OWL2DtypeVoc.XSD_INTEGER))
+
+    def test___eq__06(self):
+        """completely different"""
+        self.assertNotEqual(23, OWL2Datatype(OWL2DtypeVoc.XSD_INTEGER))
+
+    def test_accept_01(self):
+        class DummyVisitor(OWLObjectVisitor):
+            def __init__(self):
+                self.visited = False
+
+            def visit(self, axiom):
+                self.visited = True
+
+        visitor = DummyVisitor()
+        owl2datatype = OWL2Datatype(OWL2DtypeVoc.XSD_BASE_64_BINARY)
+        res = owl2datatype.accept(visitor)
+
+        self.assertIsNone(res)
+        self.assertTrue(visitor.visited)
+
+    def test_accept_02(self):
+        class DummyVisitor(OWLObjectVisitorEx):
+            def __init__(self):
+                self.return_value = 23
+                self.visited = False
+
+            def visit(self, axiom):
+                self.visited = True
+                return self.return_value
+
+        visitor = DummyVisitor()
+        owl2datatype = OWL2Datatype(OWL2DtypeVoc.XSD_BASE_64_BINARY)
+        res = owl2datatype.accept(visitor)
+
+        self.assertEqual(visitor.return_value, res)
+        self.assertTrue(visitor.visited)
+
+    def test_accept_03(self):
+        class DummyVisitor(object):
+            def __init__(self):
+                self.return_value = 23
+                self.visited = False
+
+            def visit(self, axiom):
+                self.visited = True
+                return self.return_value
+
+        visitor = DummyVisitor()
+        owl2datatype = OWL2Datatype(OWL2DtypeVoc.XSD_BASE_64_BINARY)
+
+        self.assertRaises(OWLRuntimeException, owl2datatype.accept, visitor)
+
+    def test_instance_map(self):
+        self.assertEqual(len(OWL2DtypeVoc), len(OWL2Datatype.instance_map))
+
+        for (vocab_dtype, owl2_dtype) in OWL2Datatype.instance_map.items():
+            self.assertEqual(vocab_dtype, owl2_dtype.owl2_datatype)
+
+    def test_get_datatype(self):
+        owl2_datatype = OWL2Datatype(OWL2DtypeVoc.XSD_DOUBLE)
+        self.assertEqual(owl2_datatype,
+                         OWL2Datatype.get_datatype(OWL2DtypeVoc.XSD_DOUBLE))
+
+    def test_get_built_in_type(self):
+        owl2_datatype = OWL2Datatype(OWL2DtypeVoc.XSD_DOUBLE)
+        self.assertEqual(OWL2DtypeVoc.XSD_DOUBLE,
+                         owl2_datatype.get_built_in_type())
+
+    def test_is_string(self):
+        self.assertFalse(OWL2Datatype(OWL2DtypeVoc.OWL_REAL).is_string())
+        self.assertFalse(OWL2Datatype(OWL2DtypeVoc.XSD_INTEGER).is_string())
+        self.assertTrue(OWL2Datatype(OWL2DtypeVoc.XSD_STRING).is_string())
+
+    def test_is_integer(self):
+        self.assertFalse(OWL2Datatype(OWL2DtypeVoc.OWL_RATIONAL).is_integer())
+        self.assertFalse(OWL2Datatype(OWL2DtypeVoc.XSD_DOUBLE).is_integer())
+        self.assertFalse(OWL2Datatype(OWL2DtypeVoc.XSD_INT).is_integer())
+        self.assertTrue(OWL2Datatype(OWL2DtypeVoc.XSD_INTEGER).is_integer())
+
+    def test_is_float(self):
+        self.assertFalse(OWL2Datatype(OWL2DtypeVoc.OWL_RATIONAL).is_float())
+        self.assertFalse(OWL2Datatype(OWL2DtypeVoc.XSD_DOUBLE).is_float())
+        self.assertTrue(OWL2Datatype(OWL2DtypeVoc.XSD_FLOAT).is_float())
+
+    def test_is_double(self):
+        self.assertFalse(OWL2Datatype(OWL2DtypeVoc.OWL_RATIONAL).is_double())
+        self.assertFalse(OWL2Datatype(OWL2DtypeVoc.XSD_FLOAT).is_double())
+        self.assertTrue(OWL2Datatype(OWL2DtypeVoc.XSD_DOUBLE).is_double())
+
+    def test_is_boolean(self):
+        self.assertFalse(OWL2Datatype(OWL2DtypeVoc.OWL_RATIONAL).is_boolean())
+        self.assertFalse(OWL2Datatype(OWL2DtypeVoc.XSD_INTEGER).is_boolean())
+        self.assertTrue(OWL2Datatype(OWL2DtypeVoc.XSD_BOOLEAN).is_boolean())
+
+    def test_is_rdf_plain_literal(self):
+        self.assertFalse(
+            OWL2Datatype(OWL2DtypeVoc.OWL_RATIONAL).is_rdf_plain_literal())
+        self.assertFalse(
+            OWL2Datatype(OWL2DtypeVoc.RDFS_LITERAL).is_rdf_plain_literal())
+        self.assertTrue(
+            OWL2Datatype(OWL2DtypeVoc.RDF_PLAIN_LITERAL).is_rdf_plain_literal())
+
+    def test_is_datatype(self):
+        self.assertTrue(OWL2Datatype(OWL2DtypeVoc.OWL_RATIONAL).is_datatype())
+        self.assertTrue(OWL2Datatype(OWL2DtypeVoc.XSD_INTEGER).is_datatype())
+        self.assertTrue(OWL2Datatype(OWL2DtypeVoc.XSD_STRING).is_datatype())
+
+    def test_is_top_datatype(self):
+        self.assertFalse(
+            OWL2Datatype(OWL2DtypeVoc.OWL_RATIONAL).is_top_datatype())
+        self.assertFalse(
+            OWL2Datatype(OWL2DtypeVoc.RDF_PLAIN_LITERAL).is_top_datatype())
+        self.assertTrue(
+            OWL2Datatype(OWL2DtypeVoc.RDFS_LITERAL).is_top_datatype())
+
+    def test_as_owl_datatype(self):
+        dtype = OWL2Datatype(OWL2DtypeVoc.OWL_RATIONAL)
+        self.assertEqual(dtype, dtype.as_owl_datatype())
+
+    def test_get_data_range_type(self):
+        self.assertEqual(
+            DataRangeType.DATATYPE,
+            OWL2Datatype(OWL2DtypeVoc.OWL_RATIONAL).get_data_range_type())
+        self.assertEqual(
+            DataRangeType.DATATYPE,
+            OWL2Datatype(
+                OWL2DtypeVoc.XSD_NEGATIVE_INTEGER).get_data_range_type())
+        self.assertEqual(
+            DataRangeType.DATATYPE,
+            OWL2Datatype(OWL2DtypeVoc.XSD_STRING).get_data_range_type())
+
+    def test_get_entity_type(self):
+        self.assertEqual(
+            EntityType.DATATYPE,
+            OWL2Datatype(OWL2DtypeVoc.XSD_INTEGER).get_entity_type())
+
+    def test_get_owl_entity(self):
+        entity = OWLDatatype(OWL2DtypeVoc.RDF_PLAIN_LITERAL.iri)
+        owl2_datatype = OWL2Datatype(OWL2DtypeVoc.XSD_INT)
+
+        self.assertEqual(
+            entity, owl2_datatype.get_owl_entity(EntityType.DATATYPE))
+
+    def test_is_type(self):
+        self.assertFalse(
+            OWL2Datatype(OWL2DtypeVoc.XSD_INT).is_type(
+                EntityType.ANNOTATION_PROPERTY))
+        self.assertTrue(
+            OWL2Datatype(OWL2DtypeVoc.XSD_INT).is_type(EntityType.DATATYPE))
+
+    def test_get_annotations(self):
+        self.fail('OWLOntology needs to be implemented first')
+
+    def test_get_annotation_assertion_axioms(self):
+        self.fail('OWLOntology needs to be implemented first')
+
+    def test_is_built_in(self):
+        self.assertTrue(OWL2Datatype(OWL2DtypeVoc.XSD_INT).is_built_in())
+        self.assertTrue(OWL2Datatype(OWL2DtypeVoc.OWL_RATIONAL).is_built_in())
+        self.assertTrue(OWL2Datatype(OWL2DtypeVoc.RDFS_LITERAL).is_built_in())
+
+    def test_is_owl_class(self):
+        self.assertFalse(OWL2Datatype(OWL2DtypeVoc.XSD_INT).is_owl_class())
+        self.assertFalse(OWL2Datatype(OWL2DtypeVoc.XSD_DOUBLE).is_owl_class())
+
+    def test_as_owl_class(self):
+        self.assertRaises(OWLRuntimeException,
+                          OWL2Datatype(OWL2DtypeVoc.XSD_INTEGER).as_owl_class)
+
+    def test_is_owl_object_property(self):
+        self.assertFalse(
+            OWL2Datatype(OWL2DtypeVoc.XSD_INTEGER).is_owl_object_property())
+        self.assertFalse(
+            OWL2Datatype(OWL2DtypeVoc.XSD_STRING).is_owl_object_property())
+
+    def test_as_owl_object_property(self):
+        self.assertRaises(
+            OWLRuntimeException,
+            OWL2Datatype(OWL2DtypeVoc.XSD_INTEGER).as_owl_object_property)
+
+    def test_is_owl_data_property(self):
+        self.assertFalse(
+            OWL2Datatype(OWL2DtypeVoc.XSD_INTEGER).is_owl_data_property())
+        self.assertFalse(
+            OWL2Datatype(OWL2DtypeVoc.OWL_RATIONAL).is_owl_data_property())
+
+    def test_as_owl_data_property(self):
+        self.assertRaises(
+            OWLRuntimeException,
+            OWL2Datatype(OWL2DtypeVoc.XSD_INTEGER).as_owl_data_property)
+
+    def test_is_owl_named_individual(self):
+        self.assertFalse(
+            OWL2Datatype(OWL2DtypeVoc.XSD_INT).is_owl_named_individual())
+        self.assertFalse(
+            OWL2Datatype(OWL2DtypeVoc.OWL_RATIONAL).is_owl_named_individual())
+
+    def test_as_owl_named_individual(self):
+        self.assertRaises(
+            OWLRuntimeException,
+            OWL2Datatype(OWL2DtypeVoc.XSD_INTEGER).as_owl_named_individual)
+
+    def test_is_owl_datatype(self):
+        self.assertTrue(
+            OWL2Datatype(OWL2DtypeVoc.XSD_STRING).is_owl_datatype())
+        self.assertTrue(
+            OWL2Datatype(OWL2DtypeVoc.OWL_RATIONAL).is_owl_datatype())
+
+    def test_is_owl_annotation_property(self):
+        self.assertFalse(OWL2Datatype(
+            OWL2DtypeVoc.XSD_STRING).is_owl_annotation_property())
+        self.assertFalse(OWL2Datatype(
+            OWL2DtypeVoc.OWL_RATIONAL).is_owl_annotation_property())
+
+    def test_as_owl_annotation_property(self):
+        self.assertRaises(
+            OWLRuntimeException,
+            OWL2Datatype(OWL2DtypeVoc.XSD_INTEGER).as_owl_annotation_property)
+
+    def test_to_string_id(self):
+        iri_str = str(OWL2DtypeVoc.OWL_RATIONAL.iri)
+        self.assertEqual(
+            iri_str, OWL2Datatype(OWL2DtypeVoc.OWL_RATIONAL).to_string_id())
+
+    def test_get_referencing_axioms(self):
+        self.fail('OWLOntology needs to be implemented first')
+
+    def test_get_signature(self):
+        self.assertEqual({OWL2Datatype(OWL2DtypeVoc.XSD_BOOLEAN)},
+                         OWL2Datatype(OWL2DtypeVoc.XSD_BOOLEAN).get_signature())
+
+    def test_contains_entity_in_signature(self):
+        self.assertTrue(
+            OWL2Datatype(OWL2DtypeVoc.XSD_BOOLEAN).contains_entity_in_signature(
+                OWL2Datatype(OWL2DtypeVoc.XSD_BOOLEAN)))
+        self.assertFalse(
+            OWL2Datatype(OWL2DtypeVoc.XSD_BOOLEAN).contains_entity_in_signature(
+                OWL2Datatype(OWL2DtypeVoc.XSD_INT)))
+
+    def test_get_anonymous_individuals(self):
+        self.assertEqual(
+            set(),
+            OWL2Datatype(OWL2DtypeVoc.XSD_NEGATIVE_INTEGER).
+            get_anonymous_individuals())
+
+    def test_get_classes_in_signature(self):
+        self.assertEqual(
+            set(),
+            OWL2Datatype(OWL2DtypeVoc.OWL_RATIONAL).get_classes_in_signature())
+
+    def test_get_data_properties_in_signature(self):
+        self.assertEqual(
+            set(),
+            OWL2Datatype(OWL2DtypeVoc.RDF_PLAIN_LITERAL).
+            get_data_properties_in_signature())
+
+    def test_get_object_properties_in_signature(self):
+        self.assertEqual(
+            set(),
+            OWL2Datatype(OWL2DtypeVoc.RDFS_LITERAL).
+            get_object_properties_in_signature())
+
+    def test_get_individuals_in_signature(self):
+        self.assertEqual(
+            set(),
+            OWL2Datatype(OWL2DtypeVoc.XSD_ANY_URI).
+            get_individuals_in_signature())
+
+    def test_get_datatypes_in_signature(self):
+        self.assertEqual(
+            {OWL2Datatype(OWL2DtypeVoc.OWL_REAL)},
+            OWL2Datatype(OWL2DtypeVoc.OWL_REAL).get_datatypes_in_signature())
+
+    def test_get_nested_class_expressions(self):
+        self.assertEqual(
+            set(),
+            OWL2Datatype(OWL2DtypeVoc.XSD_DOUBLE).
+            get_nested_class_expressions())
+
+    def test_is_top_entity(self):
+        self.assertFalse(OWL2Datatype(OWL2DtypeVoc.XSD_STRING).is_top_entity())
+        self.assertFalse(OWL2Datatype(OWL2DtypeVoc.XSD_FLOAT).is_top_entity())
+        self.assertTrue(
+            OWL2Datatype(OWL2DtypeVoc.RDF_PLAIN_LITERAL).is_top_entity())
+
+    def test_is_bottom_entity(self):
+        self.assertFalse(
+            OWL2Datatype(OWL2DtypeVoc.XSD_STRING).is_bottom_entity())
+        self.assertFalse(OWL2Datatype(OWL2DtypeVoc.OWL_REAL).is_bottom_entity())
+        self.assertFalse(
+            OWL2Datatype(OWL2DtypeVoc.RDF_XML_LITERAL).is_bottom_entity())
+
+
+
+
+
