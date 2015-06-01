b2d941ad81c82a9734c80219cddbfc97cf031969
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-06-01 18:16:37 +0200

    added accept methods to OWLAnn.AssertionAxiom

--- owlapy/model/owlannotationassertionaxiom.py
+++ owlapy/model/owlannotationassertionaxiom.py
@@ -1,5 +1,8 @@
 from .axiomtype import AxiomType
 from .owlaxiom import OWLAxiom
+from .owlaxiomvisitor import OWLAxiomVisitor, OWLAxiomVisitorEx
+from .owlobjectvisitor import OWLObjectVisitor, OWLObjectVisitorEx
+from owlapy.util import accept_default, accept_default_ex
 
 
 class OWLAnnotationAssertionAxiom(OWLAxiom):
@@ -19,6 +22,11 @@ class OWLAnnotationAssertionAxiom(OWLAxiom):
         self.subject = subject
         self.property = property
         self.value = value
+
+        self._accept_fn_for_visitor_cls[OWLAxiomVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLAxiomVisitorEx] = accept_default_ex
+        self._accept_fn_for_visitor_cls[OWLObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLObjectVisitorEx] = accept_default_ex
 
     @classmethod
     def get_axiom_type(cls):
