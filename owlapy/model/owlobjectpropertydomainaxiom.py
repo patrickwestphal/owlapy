627d1b182d55781316d512a72ed81bf49c7c6147
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-06-01 19:52:27 +0200

    added accept methods to OWLObj.PropertyDomainAxiom

--- owlapy/model/owlobjectpropertydomainaxiom.py
+++ owlapy/model/owlobjectpropertydomainaxiom.py
@@ -1,5 +1,8 @@
 from .axiomtype import AxiomType
+from .owlaxiomvisitor import OWLAxiomVisitor, OWLAxiomVisitorEx
+from .owlobjectvisitor import OWLObjectVisitor, OWLObjectVisitorEx
 from .owlpropertydomainaxiom import OWLPropertyDomainAxiom
+from owlapy.util import accept_default, accept_default_ex
 
 
 class OWLObjectPropertyDomainAxiom(OWLPropertyDomainAxiom):
@@ -12,6 +15,11 @@ class OWLObjectPropertyDomainAxiom(OWLPropertyDomainAxiom):
         :param annotations: s set/list of owlapy.model.OWLAnnotation objects
         """
         super().__init__(property, domain, annotations)
+
+        self._accept_fn_for_visitor_cls[OWLAxiomVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLAxiomVisitorEx] = accept_default_ex
+        self._accept_fn_for_visitor_cls[OWLObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLObjectVisitorEx] = accept_default_ex
 
     @classmethod
     def get_axiom_type(cls):
