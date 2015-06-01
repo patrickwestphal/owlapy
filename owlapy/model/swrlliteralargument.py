d95e7aa09e079e52affe412d4eebde9639888ab4
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-06-01 15:00:12 +0200

    added accept methods to SWRLLiteralArgument

--- owlapy/model/swrlliteralargument.py
+++ owlapy/model/swrlliteralargument.py
@@ -1,6 +1,9 @@
 from functools import total_ordering
 
 from .owlobject import OWLObject
+from .owlobjectvisitor import OWLObjectVisitor, OWLObjectVisitorEx
+from .swrlobjectvisitor import SWRLObjectVisitor, SWRLObjectVisitorEx
+from owlapy.util import accept_default, accept_default_ex
 
 
 @total_ordering
@@ -13,6 +16,11 @@ class SWRLLiteralArgument(OWLObject):
         """
         super().__init__()
         self.literal = literal
+
+        self._accept_fn_for_visitor_cls[OWLObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLObjectVisitorEx] = accept_default_ex
+        self._accept_fn_for_visitor_cls[SWRLObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[SWRLObjectVisitorEx] = accept_default_ex
 
     def __eq__(self, other):
         if id(self) == id(other):
