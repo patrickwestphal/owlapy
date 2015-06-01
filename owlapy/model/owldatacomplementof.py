0df4b685e56db2105b04d3e7f903c7067dd907a5
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-06-01 15:51:32 +0200

    added accept methods to OWLDataComplementOf

--- owlapy/model/owldatacomplementof.py
+++ owlapy/model/owldatacomplementof.py
@@ -1,4 +1,8 @@
+from .owldatarangevisitor import OWLDataRangeVisitor, OWLDataRangeVisitorEx
+from .owldatavisitor import OWLDataVisitor, OWLDataVisitorEx
 from .owlobject import OWLObject
+from .owlobjectvisitor import OWLObjectVisitor, OWLObjectVisitorEx
+from owlapy.util import accept_default, accept_default_ex
 
 
 class OWLDataComplementOf(OWLObject):
@@ -10,3 +14,11 @@ class OWLDataComplementOf(OWLObject):
         """
         super().__init__()
         self.data_range = data_range
+
+        self._accept_fn_for_visitor_cls[OWLDataRangeVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLDataRangeVisitorEx] = \
+            accept_default_ex
+        self._accept_fn_for_visitor_cls[OWLDataVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLDataVisitorEx] = accept_default_ex
+        self._accept_fn_for_visitor_cls[OWLObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLObjectVisitorEx] = accept_default_ex
