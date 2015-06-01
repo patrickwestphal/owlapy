4a600d4f1cc0d4169e0b53c038c94544ccdc29ec
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-06-01 17:28:37 +0200

    added accept methods to OWLSameIndividualAxiom

--- owlapy/model/owlsameindividualaxiom.py
+++ owlapy/model/owlsameindividualaxiom.py
@@ -1,5 +1,8 @@
 from .axiomtype import AxiomType
+from .owlaxiomvisitor import OWLAxiomVisitor, OWLAxiomVisitorEx
 from .owlnaryindividualaxiom import OWLNaryIndividualAxiom
+from .owlobjectvisitor import OWLObjectVisitor, OWLObjectVisitorEx
+from owlapy.util import accept_default, accept_default_ex
 
 
 class OWLSameIndividualAxiom(OWLNaryIndividualAxiom):
@@ -11,6 +14,11 @@ class OWLSameIndividualAxiom(OWLNaryIndividualAxiom):
         :param annotations: a set/list of owlapy.model.OWLAnnotation objects
         """
         super().__init__(individuals, annotations)
+
+        self._accept_fn_for_visitor_cls[OWLAxiomVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLAxiomVisitorEx] = accept_default_ex
+        self._accept_fn_for_visitor_cls[OWLObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLObjectVisitorEx] = accept_default_ex
 
     @classmethod
     def get_axiom_type(cls):
