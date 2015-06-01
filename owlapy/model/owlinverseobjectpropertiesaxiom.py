dab8c6da20d3058baaed2dd2ba4d5d86ca873113
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-06-01 17:18:28 +0200

    added accept methods to OWLInv.Obj.PropertiesAxiom

--- owlapy/model/owlinverseobjectpropertiesaxiom.py
+++ owlapy/model/owlinverseobjectpropertiesaxiom.py
@@ -1,5 +1,8 @@
 from .axiomtype import AxiomType
+from .owlaxiomvisitor import OWLAxiomVisitor, OWLAxiomVisitorEx
 from .owlnarypropertyaxiom import OWLNaryPropertyAxiom
+from .owlobjectvisitor import OWLObjectVisitor, OWLObjectVisitorEx
+from owlapy.util import accept_default, accept_default_ex
 
 
 class OWLInverseObjectPropertiesAxiom(OWLNaryPropertyAxiom):
@@ -14,6 +17,11 @@ class OWLInverseObjectPropertiesAxiom(OWLNaryPropertyAxiom):
         super().__init__([first, second], annotations)
         self.first_property = first
         self.second_property = second
+
+        self._accept_fn_for_visitor_cls[OWLAxiomVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLAxiomVisitorEx] = accept_default_ex
+        self._accept_fn_for_visitor_cls[OWLObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLObjectVisitorEx] = accept_default_ex
 
     @classmethod
     def get_axiom_type(cls):
