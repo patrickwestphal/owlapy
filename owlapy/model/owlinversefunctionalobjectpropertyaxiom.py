c832adfbc2d10c7b1c8bfea4fadd0c64bc7c283d
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-06-01 17:31:31 +0200

    added accept methods to OWLInv.Func.Obj.Prop.Axiom

--- owlapy/model/owlinversefunctionalobjectpropertyaxiom.py
+++ owlapy/model/owlinversefunctionalobjectpropertyaxiom.py
@@ -1,9 +1,13 @@
 from .axiomtype import AxiomType
+from .owlaxiomvisitor import OWLAxiomVisitor, OWLAxiomVisitorEx
 from .owlobjectpropertycharacteristicaxiom import \
     OWLObjectPropertyCharacteristicAxiom
+from .owlobjectvisitor import OWLObjectVisitor, OWLObjectVisitorEx
+from owlapy.util import accept_default, accept_default_ex
 
 
-class OWLInverseFunctionalObjectPropertyAxiom(OWLObjectPropertyCharacteristicAxiom):
+class OWLInverseFunctionalObjectPropertyAxiom(
+        OWLObjectPropertyCharacteristicAxiom):
     """TODO: implement"""
 
     def __init__(self, property, annotations):
@@ -12,6 +16,11 @@ class OWLInverseFunctionalObjectPropertyAxiom(OWLObjectPropertyCharacteristicAxi
         :param annotations: a set/list of owlapy.model.OWLAnnotation objects
         """
         super().__init__(property, annotations)
+
+        self._accept_fn_for_visitor_cls[OWLAxiomVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLAxiomVisitorEx] = accept_default_ex
+        self._accept_fn_for_visitor_cls[OWLObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLObjectVisitorEx] = accept_default_ex
 
     @classmethod
     def get_axiom_type(cls):
