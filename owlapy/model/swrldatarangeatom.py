1e8d06d022312779593df93eda7a613bea66d883
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-06-01 15:22:45 +0200

    added accept methods to SWRLDataRangeAtom

--- owlapy/model/swrldatarangeatom.py
+++ owlapy/model/swrldatarangeatom.py
@@ -1,4 +1,7 @@
+from .owlobjectvisitor import OWLObjectVisitor, OWLObjectVisitorEx
 from .swrlunaryatom import SWRLUnaryAtom
+from .swrlobjectvisitor import SWRLObjectVisitor, SWRLObjectVisitorEx
+from owlapy.util import accept_default, accept_default_ex
 
 
 class SWRLDataRangeAtom(SWRLUnaryAtom):
@@ -10,3 +13,8 @@ class SWRLDataRangeAtom(SWRLUnaryAtom):
         :param arg: an owlapy.model.SWRLDArgument object
         """
         super().__init__(predicate, arg)
+
+        self._accept_fn_for_visitor_cls[OWLObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLObjectVisitorEx] = accept_default_ex
+        self._accept_fn_for_visitor_cls[SWRLObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[SWRLObjectVisitorEx] = accept_default_ex
