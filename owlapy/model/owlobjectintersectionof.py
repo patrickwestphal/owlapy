1d0892bdc15508e49156a1328bc6257000ef9ca0
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-06-01 17:08:26 +0200

    added accept methods to OWLObjectIntersectionOf

--- owlapy/model/owlobjectintersectionof.py
+++ owlapy/model/owlobjectintersectionof.py
@@ -1,4 +1,8 @@
+from .owlclassexpressionvisitor import OWLClassExpressionVisitor, \
+    OWLClassExpressionVisitorEx
 from .owlnarybooleanclassexpression import OWLNaryBooleanClassExpression
+from .owlobjectvisitor import OWLObjectVisitor, OWLObjectVisitorEx
+from owlapy.util import accept_default, accept_default_ex
 
 
 class OWLObjectIntersectionOf(OWLNaryBooleanClassExpression):
@@ -9,3 +13,10 @@ class OWLObjectIntersectionOf(OWLNaryBooleanClassExpression):
         :param operands: a set of owlapy.model.OWLClassExpression objects
         """
         super().__init__(operands)
+
+        self._accept_fn_for_visitor_cls[OWLClassExpressionVisitor] = \
+            accept_default
+        self._accept_fn_for_visitor_cls[OWLClassExpressionVisitorEx] = \
+            accept_default_ex
+        self._accept_fn_for_visitor_cls[OWLObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLObjectVisitorEx] = accept_default_ex
