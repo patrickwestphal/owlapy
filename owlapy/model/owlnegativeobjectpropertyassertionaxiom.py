ee0aabd016c144ca4072c5c93c77cec96bdc00b2
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-06-01 20:07:20 +0200

    added accept methods to OWLNeg.Obj.Prop.Ass.Axiom

--- owlapy/model/owlnegativeobjectpropertyassertionaxiom.py
+++ owlapy/model/owlnegativeobjectpropertyassertionaxiom.py
@@ -1,4 +1,7 @@
+from .owlaxiomvisitor import OWLAxiomVisitor, OWLAxiomVisitorEx
+from .owlobjectvisitor import OWLObjectVisitor, OWLObjectVisitorEx
 from .owlpropertyassertionaxiom import OWLPropertyAssertionAxiom
+from owlapy.util import accept_default, accept_default_ex
 
 
 class OWLNegativeObjectPropertyAssertionAxiom(OWLPropertyAssertionAxiom):
@@ -12,3 +15,8 @@ class OWLNegativeObjectPropertyAssertionAxiom(OWLPropertyAssertionAxiom):
         :param annotations: a list/set of owlapy.model.OWLAnnotation objects
         """
         super().__init__(subject, property, object, annotations)
+
+        self._accept_fn_for_visitor_cls[OWLAxiomVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLAxiomVisitorEx] = accept_default_ex
+        self._accept_fn_for_visitor_cls[OWLObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLObjectVisitorEx] = accept_default_ex
