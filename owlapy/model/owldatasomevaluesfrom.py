97dabebf846e03b1acac41c8215d8ed8580929fb
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-06-01 16:33:04 +0200

    added accept methods to OWLDataSomeValuesFrom

--- owlapy/model/owldatasomevaluesfrom.py
+++ owlapy/model/owldatasomevaluesfrom.py
@@ -1,4 +1,8 @@
+from .owlclassexpressionvisitor import OWLClassExpressionVisitor, \
+    OWLClassExpressionVisitorEx
+from .owlobjectvisitor import OWLObjectVisitor, OWLObjectVisitorEx
 from .owlquantifieddatarestriction import OWLQuantifiedDataRestriction
+from owlapy.util import accept_default, accept_default_ex
 
 
 class OWLDataSomeValuesFrom(OWLQuantifiedDataRestriction):
@@ -10,3 +14,10 @@ class OWLDataSomeValuesFrom(OWLQuantifiedDataRestriction):
         :param filler: an owlapy.model.OWLDataRange object
         """
         super().__init__(property, filler)
+
+        self._accept_fn_for_visitor_cls[OWLClassExpressionVisitor] = \
+            accept_default
+        self._accept_fn_for_visitor_cls[OWLClassExpressionVisitorEx] = \
+            accept_default_ex
+        self._accept_fn_for_visitor_cls[OWLObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLObjectVisitorEx] = accept_default_ex
