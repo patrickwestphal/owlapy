5e33aec4e4bc12d5a25d98bdb1a95d60f1e8305c
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-06-01 15:08:20 +0200

    added accept methods for SWRLIndividualArgument

--- owlapy/model/swrlindividualargument.py
+++ owlapy/model/swrlindividualargument.py
@@ -1,4 +1,7 @@
 from .owlobject import OWLObject
+from .owlobjectvisitor import OWLObjectVisitor, OWLObjectVisitorEx
+from .swrlobjectvisitor import SWRLObjectVisitor, SWRLObjectVisitorEx
+from owlapy.util import accept_default_ex, accept_default
 
 
 class SWRLIndividualArgument(OWLObject):
@@ -10,3 +13,8 @@ class SWRLIndividualArgument(OWLObject):
         """
         super().__init__()
         self.individual = individual
+
+        self._accept_fn_for_visitor_cls[OWLObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLObjectVisitorEx] = accept_default_ex
+        self._accept_fn_for_visitor_cls[SWRLObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[SWRLObjectVisitorEx] = accept_default_ex
