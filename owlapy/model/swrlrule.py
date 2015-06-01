a55c01e9824f317cd19569e066c47970584598af
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-06-01 17:15:50 +0200

    added accept methods to SWRLRule

--- owlapy/model/swrlrule.py
+++ owlapy/model/swrlrule.py
@@ -1,5 +1,9 @@
 from .axiomtype import AxiomType
+from .owlaxiomvisitor import OWLAxiomVisitor, OWLAxiomVisitorEx
 from .owllogicalaxiom import OWLLogicalAxiom
+from .owlobjectvisitor import OWLObjectVisitor, OWLObjectVisitorEx
+from .swrlobjectvisitor import SWRLObjectVisitor, SWRLObjectVisitorEx
+from owlapy.util import accept_default, accept_default_ex
 
 
 class SWRLRule(OWLLogicalAxiom):
@@ -15,6 +19,13 @@ class SWRLRule(OWLLogicalAxiom):
         self.head = head
         self.body = body
         # containsAnonymousClassExpressions = hasAnon();
+
+        self._accept_fn_for_visitor_cls[OWLAxiomVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLAxiomVisitorEx] = accept_default_ex
+        self._accept_fn_for_visitor_cls[OWLObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLObjectVisitorEx] = accept_default_ex
+        self._accept_fn_for_visitor_cls[SWRLObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[SWRLObjectVisitorEx] = accept_default_ex
 
     @classmethod
     def get_axiom_type(cls):
