35f045e6faf6eedbcf115c9b6d17a98741e887b7
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-06-01 16:47:36 +0200

    added accept methods to OWLObjectHasValue

--- owlapy/model/owlobjecthasvalue.py
+++ owlapy/model/owlobjecthasvalue.py
@@ -1,4 +1,8 @@
+from .owlclassexpressionvisitor import OWLClassExpressionVisitor, \
+    OWLClassExpressionVisitorEx
+from .owlobjectvisitor import OWLObjectVisitor, OWLObjectVisitorEx
 from .owlvaluerestriction import OWLValueRestriction
+from owlapy.util import accept_default, accept_default_ex
 
 
 class OWLObjectHasValue(OWLValueRestriction):
@@ -10,3 +14,10 @@ class OWLObjectHasValue(OWLValueRestriction):
         :param value: an owlapy.model.OWLIndividual value
         """
         super().__init__(property, value)
+
+        self._accept_fn_for_visitor_cls[OWLClassExpressionVisitor] = \
+            accept_default
+        self._accept_fn_for_visitor_cls[OWLClassExpressionVisitorEx] = \
+            accept_default_ex
+        self._accept_fn_for_visitor_cls[OWLObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLObjectVisitorEx] = accept_default_ex
