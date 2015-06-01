ae983aea1e9ada0af81848275d5c252c56e15a11
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-06-01 16:38:56 +0200

    added accept methods to OWLObjectMaxCardinality

--- owlapy/model/owlobjectmaxcardinality.py
+++ owlapy/model/owlobjectmaxcardinality.py
@@ -1,4 +1,8 @@
+from .owlclassexpressionvisitor import OWLClassExpressionVisitor, \
+    OWLClassExpressionVisitorEx
 from .owlobjectcardinalityrestriction import OWLObjectCardinalityRestriction
+from .owlobjectvisitor import OWLObjectVisitor, OWLObjectVisitorEx
+from owlapy.util import accept_default, accept_default_ex
 
 
 class OWLObjectMaxCardinality(OWLObjectCardinalityRestriction):
@@ -11,3 +15,10 @@ class OWLObjectMaxCardinality(OWLObjectCardinalityRestriction):
         :param filler: an owlapy.model.OWLClassExpression
         """
         super().__init__(property, cardinality, filler)
+
+        self._accept_fn_for_visitor_cls[OWLClassExpressionVisitor] = \
+            accept_default
+        self._accept_fn_for_visitor_cls[OWLClassExpressionVisitorEx] = \
+            accept_default_ex
+        self._accept_fn_for_visitor_cls[OWLObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLObjectVisitorEx] = accept_default_ex
