26c8d1a846d5402e09fda1f97a253cd943f6459c
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-06-01 18:19:22 +0200

    added accept methods to OWLDeclarationAxiom

--- owlapy/model/owldeclarationaxiom.py
+++ owlapy/model/owldeclarationaxiom.py
@@ -1,5 +1,8 @@
 from .axiomtype import AxiomType
 from .owlaxiom import OWLAxiom
+from .owlaxiomvisitor import OWLAxiomVisitor, OWLAxiomVisitorEx
+from .owlobjectvisitor import OWLObjectVisitor, OWLObjectVisitorEx
+from owlapy.util import accept_default, accept_default_ex
 
 
 class OWLDeclarationAxiom(OWLAxiom):
@@ -12,6 +15,11 @@ class OWLDeclarationAxiom(OWLAxiom):
         """
         super().__init__(annotations)
         self.entity = entity
+
+        self._accept_fn_for_visitor_cls[OWLAxiomVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLAxiomVisitorEx] = accept_default_ex
+        self._accept_fn_for_visitor_cls[OWLObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLObjectVisitorEx] = accept_default_ex
 
     @classmethod
     def get_axiom_type(cls):
