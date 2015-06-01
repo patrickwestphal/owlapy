2dcb3ff27f084f7184c75a72ea70e206b9cbbb9d
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-06-01 17:24:28 +0200

    added accept methods to OWLSubPropertyChainOfAxiom

--- owlapy/model/owlsubpropertychainofaxiom.py
+++ owlapy/model/owlsubpropertychainofaxiom.py
@@ -1,5 +1,8 @@
 from .axiomtype import AxiomType
+from .owlaxiomvisitor import OWLAxiomVisitor, OWLAxiomVisitorEx
+from .owlobjectvisitor import OWLObjectVisitor, OWLObjectVisitorEx
 from .owlpropertyaxiom import OWLPropertyAxiom
+from owlapy.util import accept_default, accept_default_ex
 
 
 class OWLSubPropertyChainOfAxiom(OWLPropertyAxiom):
@@ -16,6 +19,11 @@ class OWLSubPropertyChainOfAxiom(OWLPropertyAxiom):
         super().__init__(annotations)
         self.property_chain = property_chain
         self.super_property = super_property
+
+        self._accept_fn_for_visitor_cls[OWLAxiomVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLAxiomVisitorEx] = accept_default_ex
+        self._accept_fn_for_visitor_cls[OWLObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLObjectVisitorEx] = accept_default_ex
 
     @classmethod
     def get_axiom_type(cls):
