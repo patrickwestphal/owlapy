874168f1b717d1d9522b18cfe6995c099b117bc5
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-06-01 20:10:19 +0200

    added accept methods to OWLSubClassOfAxiom

--- owlapy/model/owlsubclassofaxiom.py
+++ owlapy/model/owlsubclassofaxiom.py
@@ -1,4 +1,7 @@
+from .owlaxiomvisitor import OWLAxiomVisitor, OWLAxiomVisitorEx
 from .owlclassaxiom import OWLClassAxiom
+from .owlobjectvisitor import OWLObjectVisitor, OWLObjectVisitorEx
+from owlapy.util import accept_default, accept_default_ex
 
 
 class OWLSubClassOfAxiom(OWLClassAxiom):
@@ -13,3 +16,8 @@ class OWLSubClassOfAxiom(OWLClassAxiom):
         super().__init__(annotations)
         self.sub_class = sub_class
         self.super_class = super_class
+
+        self._accept_fn_for_visitor_cls[OWLAxiomVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLAxiomVisitorEx] = accept_default_ex
+        self._accept_fn_for_visitor_cls[OWLObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLObjectVisitorEx] = accept_default_ex
