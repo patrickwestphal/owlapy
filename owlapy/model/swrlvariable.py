19e530ba49579319fc805fad2314abdc1b5fb861
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-06-01 15:11:18 +0200

    added accept methods for owlapy.model.SWRLVariable

--- owlapy/model/swrlvariable.py
+++ owlapy/model/swrlvariable.py
@@ -1,4 +1,7 @@
 from .owlobject import OWLObject
+from .owlobjectvisitor import OWLObjectVisitor, OWLObjectVisitorEx
+from .swrlobjectvisitor import SWRLObjectVisitor, SWRLObjectVisitorEx
+from owlapy.util import accept_default, accept_default_ex
 
 
 class SWRLVariable(OWLObject):
@@ -10,3 +13,8 @@ class SWRLVariable(OWLObject):
         """
         super().__init__()
         self.iri = iri
+
+        self._accept_fn_for_visitor_cls[OWLObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLObjectVisitorEx] = accept_default_ex
+        self._accept_fn_for_visitor_cls[SWRLObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[SWRLObjectVisitorEx] = accept_default_ex
