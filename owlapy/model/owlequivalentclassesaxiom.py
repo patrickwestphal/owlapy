a5109499d4899829d2662ae2eea1fc0bbe2afe2f
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-06-01 17:47:46 +0200

    added accept methods to OWLEquivalentClassesAxiom

--- owlapy/model/owlequivalentclassesaxiom.py
+++ owlapy/model/owlequivalentclassesaxiom.py
@@ -1,5 +1,8 @@
 from .axiomtype import AxiomType
+from .owlaxiomvisitor import OWLAxiomVisitor, OWLAxiomVisitorEx
 from .owlnaryclassaxiom import OWLNaryClassAxiom
+from .owlobjectvisitor import OWLObjectVisitor, OWLObjectVisitorEx
+from owlapy.util import accept_default, accept_default_ex
 
 
 class OWLEquivalentClassesAxiom(OWLNaryClassAxiom):
@@ -13,6 +16,11 @@ class OWLEquivalentClassesAxiom(OWLNaryClassAxiom):
         """
         super().__init__(class_expressions, annotations)
         self.named_classes = None
+
+        self._accept_fn_for_visitor_cls[OWLAxiomVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLAxiomVisitorEx] = accept_default_ex
+        self._accept_fn_for_visitor_cls[OWLObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLObjectVisitorEx] = accept_default_ex
 
     @classmethod
     def get_axiom_type(cls):
