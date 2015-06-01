5fcf2fdf6c8030cf8486adcbd2e102f85fbf8017
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-06-01 18:06:04 +0200

    added accept methods to OWLAnn.PropertyRangeAxiom

--- owlapy/model/owlannotationpropertyrangeaxiom.py
+++ owlapy/model/owlannotationpropertyrangeaxiom.py
@@ -1,5 +1,8 @@
 from .axiomtype import AxiomType
 from .owlaxiom import OWLAxiom
+from .owlaxiomvisitor import OWLAxiomVisitor, OWLAxiomVisitorEx
+from .owlobjectvisitor import OWLObjectVisitor, OWLObjectVisitorEx
+from owlapy.util import accept_default, accept_default_ex
 
 
 class OWLAnnotationPropertyRangeAxiom(OWLAxiom):
@@ -14,6 +17,11 @@ class OWLAnnotationPropertyRangeAxiom(OWLAxiom):
         super().__init__(annotations)
         self.property = property
         self.range = range
+
+        self._accept_fn_for_visitor_cls[OWLAxiomVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLAxiomVisitorEx] = accept_default_ex
+        self._accept_fn_for_visitor_cls[OWLObjectVisitor] = accept_default
+        self._accept_fn_for_visitor_cls[OWLObjectVisitorEx] = accept_default_ex
 
     @classmethod
     def get_axiom_type(cls):
