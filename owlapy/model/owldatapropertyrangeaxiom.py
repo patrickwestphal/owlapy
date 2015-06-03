09846e246c0e206ae2f9892d62242bee045afe33
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-06-03 19:08:56 +0200

    updated doc str in OWLDataPropertyRangeAxiom

--- owlapy/model/owldatapropertyrangeaxiom.py
+++ owlapy/model/owldatapropertyrangeaxiom.py
@@ -11,7 +11,11 @@ class OWLDataPropertyRangeAxiom(OWLPropertyRangeAxiom):
     def __init__(self, property, range, annotations):
         """
         :param property: an owlapy.model.OWLDataPropertyExpression object
-        :param range: an owlapy.model.OWLDataRange object
+        :param range: an owlapy.model.OWLDataRange object (i,e, an
+            owlapy.model.OWLDataComplementOf object, an
+            owlapy.model.OWLDataOneOf object, an owlapy.model.OWLDatatype
+            object, an owlapy.model.OWLDatatypeRestriction object, or an
+            owlapy.model.OWLNaryDataRange object)
         :param annotations: a set/list of owlapy.model.OWLAnnotation objects
         :return:
         """
