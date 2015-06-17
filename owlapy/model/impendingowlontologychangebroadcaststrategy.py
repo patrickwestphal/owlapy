237a533bbec72b94a1e751fb6c458937e8e46e0a
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-06-17 22:13:17 +0200

    added forgotten parameter to impendingowlont...

--- owlapy/model/impendingowlontologychangebroadcaststrategy.py
+++ owlapy/model/impendingowlontologychangebroadcaststrategy.py
@@ -1,9 +1,12 @@
 class ImpendingOWLOntologyChangeBroadcastStrategy(object):
     """Marker class"""
 
-    def broadcast_changes(self, changes):
+    def broadcast_changes(self, listener, changes):
         """Broadcasts the list of changes to the specified listeners.
 
+        :param listener: the listener, i.e. the
+            owlapy.model.ImpendingOWLOntologyChangeListener object the changes
+            should be broadcasted to
         :param changes: The changes, i.e. list of owlapy.model.OWLOntologyChange
             objects to be broadcast.
         """
